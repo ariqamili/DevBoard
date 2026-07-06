24c261ec9d160cb0290ffd518e58657253e18721
Arian Qamili <aq32036@seeu.edu.mk>
2026-07-06 17:50:35 +0200

    feat: add React Router setup with all page routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,22 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Job from "./pages/Job";
+import JobDetail from "./pages/JobDetail";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
-    <div>
-      <h1>DevBoard</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/jobs" element={<Job />} />
+      <Route path="/jobs/:id" element={<JobDetail />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
   );
 }
 
