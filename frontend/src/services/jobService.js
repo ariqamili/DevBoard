582c8f9f5c26d7449d6e24be7b6c9d5022749d55
Arian Qamili <aq32036@seeu.edu.mk>
2026-07-26 02:55:04 +0200

    fix: update jobService to import from new api/axios module

--- frontend/src/services/jobService.js
+++ frontend/src/services/jobService.js
@@ -1,9 +1,15 @@
-import api from "./api";
+import api from "../api/axios";
 
-const getJobs = async () => (await api.get('/jobs')).data
-const getJob = async (id) => (await api.get(`/jobs/${id}`)).data
-const createJob = async (data) => (await api.post('/jobs', data)).data
-const updateJob = async (id, data) => (await api.put(`/jobs/${id}`, data)).data
-const deleteJob = async (id) => (await api.delete(`/jobs/${id}`)).data
+const getJobs = async () => (await api.get("/jobs")).data;
+const getJob = async (id) => (await api.get(`/jobs/${id}`)).data;
+const createJob = async (data) => (await api.post("/jobs", data)).data;
+const updateJob = async (id, data) => (await api.put(`/jobs/${id}`, data)).data;
+const deleteJob = async (id) => (await api.delete(`/jobs/${id}`)).data;
 
-export {getJobs, getJob, createJob, updateJob, deleteJob}
+export default {
+  getJobs,
+  getJob,
+  createJob,
+  updateJob,
+  deleteJob,
+};
